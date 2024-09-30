cc7c4ebdf5f7f25e404dba7e9d4661ee6a242d7c
Manjunathan <manjunathanj002@gmail.com>
2024-09-30 14:12:54 +0530

    project structure with basic dependencies

---
+++ src/main/java/com/ecommerceproject/gamezone/GamezoneApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerceproject.gamezone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamezoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamezoneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecommerceproject/gamezone/GamezoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerceproject.gamezone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamezoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
